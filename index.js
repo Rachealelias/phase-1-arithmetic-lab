0bc45b063fa1ab7099120d6d89ea2cdf017485ec
Rachealelias <rachealsamuel708@gmail.com>
2021-06-30 19:35:56 -0400

    commit tree f7a6bf7113f2873eb8547c8fc2060e46b79dc127

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 5;
+Number.isInteger(random);
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max =(10,15,20);
+Math.max(max);
